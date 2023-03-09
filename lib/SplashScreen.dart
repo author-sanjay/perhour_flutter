6449feb5ef153e4871a7be25d68e6d8f8e5a6973
sanjay <sanjaykumar73189@gmail.com>
2023-03-09 18:55:36 +0530

    cleanup

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names, unnecessary_brace_in_string_interps, avoid_print, use_build_context_synchronously, sized_box_for_whitespace
+
 import 'dart:async';
 import 'dart:convert';
 
