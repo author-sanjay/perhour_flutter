7df7df3620924bf1dbdffeff9c1322424e0422b7
sanjay <sanjaykumar73189@gmail.com>
2023-03-13 12:05:58 +0530

    cleanupp

--- lib/Screens/Withdraw/Withdraw.dart
+++ lib/Screens/Withdraw/Withdraw.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names, sized_box_for_whitespace, empty_statements
+
 import 'package:flutter/material.dart';
 import 'package:perhour_flutter/Colors.dart';
 import 'package:perhour_flutter/Screens/ListBids/ListBids.dart';
@@ -19,7 +21,7 @@ class _WithdrawState extends State<Withdraw> {
           padding: const EdgeInsets.all(18.0),
           child: Topbar(title: "Withdraw"),
         ),
-        Container(decoration: BoxDecoration(color: Color.fromARGB(
+        Container(decoration: BoxDecoration(color: const Color.fromARGB(
             247, 245, 223, 123), borderRadius: BorderRadius.circular(20)),width: MediaQuery.of(context).size.width*0.9,child: Padding(
               padding: const EdgeInsets.all(30.0),
 
@@ -30,30 +32,30 @@ class _WithdrawState extends State<Withdraw> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Enter Amount",style: TextStyle(fontSize: 18),),
-                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: InputDecoration(disabledBorder: InputBorder.none),)),
-                        Padding(
-                          padding: const EdgeInsets.only(top:18.0),
+                        const Text("Enter Amount",style: TextStyle(fontSize: 18),),
+                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: const InputDecoration(disabledBorder: InputBorder.none),)),
+                        const Padding(
+                          padding: EdgeInsets.only(top:18.0),
                           child: Text("Paypal/Upi",style: TextStyle(fontSize: 18),),
                         ),
-                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: InputDecoration(disabledBorder: InputBorder.none),)),
-                        Padding(
-                          padding: const EdgeInsets.only(top:18.0),
+                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: const InputDecoration(disabledBorder: InputBorder.none),)),
+                        const Padding(
+                          padding: EdgeInsets.only(top:18.0),
                           child: Text("Banking Name",style: TextStyle(fontSize: 18),),
                         ),
-                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: InputDecoration(disabledBorder: InputBorder.none),))
-                        , Padding(
-                          padding: const EdgeInsets.only(top:18.0),
+                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: const InputDecoration(disabledBorder: InputBorder.none),))
+                        , const Padding(
+                          padding: EdgeInsets.only(top:18.0),
                           child: Text("Enter Paypal/Upi Ac number",style: TextStyle(fontSize: 18),),
                         ),
-                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: InputDecoration(disabledBorder: InputBorder.none),)),
+                        Container(height: MediaQuery.of(context).size.height*0.04,child: TextFormField(decoration: const InputDecoration(disabledBorder: InputBorder.none),)),
 
                       ],
                     ),
                     Padding(
                       padding: const EdgeInsets.only(top:18.0),
-                      child: GestureDetector(onTap: (){},child: Container(width: MediaQuery.of(context).size.width*0.9, decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10)),child: Padding(
-                        padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(onTap: (){},child: Container(width: MediaQuery.of(context).size.width*0.9, decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10)),child: const Padding(
+                        padding: EdgeInsets.all(8.0),
                         child: Center(child: Text("Withdraw",style: TextStyle(fontSize: 18, color: Colors.white),)),
                       ),),),
                     )
@@ -62,6 +64,6 @@ class _WithdrawState extends State<Withdraw> {
               ),
             ),)
       ],
-    ),),),);;
+    ),),),);
   }
 }
